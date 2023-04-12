1b2f73b33fb824ef236990a40b7a7be2a0e0ed20
lennard <lennard.schuette@googlemail.com>
2023-04-12 21:57:04 +0200

    added Site class

--- ssg/site.py
+++ ssg/site.py
@@ -3,18 +3,17 @@ from pathlib import Path
 
 class Site:
 
-    def __init__(self, src: str, dest: str):
-        self.source = Path(src)
-        self.destination = Path(dest)
+    def __init__(self, source: str, dest: str):
+        self.source = Path(source)
+        self.dest = Path(dest)
 
     def create_dir(self, path: Path) -> None:
-        directory: Path = self.destination / path.relative_to(self.source)
+        directory: Path = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
 
     def build(self) -> None:
-        self.destination.mkdir(parents=True, exist_ok=True)
+        self.dest.mkdir(parents=True, exist_ok=True)
 
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
-
