43e8f723c0b0d0c14995f191bd26382331c09e75
lennard <lennard.schuette@googlemail.com>
2023-04-13 10:24:36 +0200

    added Parser class

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -18,7 +18,7 @@ class Parser:
             return file.read()
 
     def write(self, path: Path, dest: Path, content: str, ext: str = '.html'):
-        full_path = self.dest / path.with_suffix(ext).name
+        full_path = dest / path.with_suffix(ext).name
         with open(full_path, 'w') as file:
             file.write(content)
 
