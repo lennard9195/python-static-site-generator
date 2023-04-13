ea21ba73625797e989fe1944eb7df2949c83fde7
lennard <lennard.schuette@googlemail.com>
2023-04-13 11:14:57 +0200

    Content Classs

--- ssg.py
+++ ssg.py
@@ -7,5 +7,4 @@ def main(source: str = "content", dest: str = "dist"):
     config = {"source": source, "dest": dest, "parsers": [ssg.parsers.ResourceParser()]}
     Site(**config).build()
 
-
 typer.run(main)
