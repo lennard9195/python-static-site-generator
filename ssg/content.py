757785d54355e286a460c5f86a9faf5050d0bcd3
lennard <lennard.schuette@googlemail.com>
2023-04-13 11:09:43 +0200

    Content Classs

--- ssg/content.py
+++ ssg/content.py
@@ -1,16 +1,17 @@
 import re
 from yaml import load
 from yaml import FullLoader
-from _collections_abc import Mapping
+from collections.abc import Mapping
 
 
 class Content(Mapping):
 
-    __delimeter = r"^(?:-|\+){3}\s*$"
-    __regex = re.compile(__delimeter, re.MULTILINE)
+    __delimiter = r"^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
 
-    def load(self, cls, string):
-        _, fm, content = self.__regex.split(string, 2)
+    @classmethod
+    def load(cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
         metadata = load(fm, Loader=FullLoader)
         return cls(metadata, content)
 
